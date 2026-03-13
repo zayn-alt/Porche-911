5835f39b4723fd3ab528abce7bca672026df8297
zayn-alt <zaynelmawla88@gmail.com>
2026-03-13 06:12:55 +0200

    fix syntax error

--- test_playwright.js
+++ test_playwright.js
@@ -1,26 +1,14 @@
 const { chromium } = require('playwright');
 
 (async () => {
-
   const URL = "https://gumyfui.com?directlink=1&code_type=1&sid=941721";
-
   console.log('جاري فتح الموقع:', URL);
-
   const browser = await chromium.launch({ headless: true });
   const page = await browser.newPage();
-
   await page.goto(URL, { waitUntil: 'networkidle' });
-
   const title = await page.title();
   console.log('عنوان الصفحة:', title);
-
-  const currentURL = page.url();
-  console.log('الرابط الحالي:', currentURL);
-
   await page.screenshot({ path: 'screenshot.png', fullPage: true });
-  console.log('تم حفظ screenshot.png');
-
   await browser.close();
   console.log('تم بنجاح');
-
 })();
